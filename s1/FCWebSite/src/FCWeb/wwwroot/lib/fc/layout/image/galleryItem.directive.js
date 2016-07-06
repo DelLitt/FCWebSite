494df419c5df9099a8f3789267c50d7938b42173
Eduard Karulis <karulis@mail.ru>
2016-07-06 23:44:57 +0000

    Multifiles upload
    git-tfs-id: [https://karulis.visualstudio.com/DefaultCollection]$/SFCSlutskWebSite3;C62

---
+++ s1/FCWebSite/src/FCWeb/wwwroot/lib/fc/layout/image/galleryItem.directive.js
@@ -0,0 +1,24 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('fc.ui')
+        .directive('galleryItem', galleryItem);
+
+    function galleryItem() {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope:
+                {
+                    model: '=',
+                    showHeader: "@"
+                },
+
+            link: function link(scope, element, attrs) {
+                scope.showHeader = false;
+            },
+            templateUrl: '/lib/fc/layout/image/galleryItem.html'
+        }
+    }
+})();
