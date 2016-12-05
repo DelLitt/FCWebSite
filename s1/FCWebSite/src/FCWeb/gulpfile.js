880588101d2702da1bbe16e8ed91a28607bfaa15
Eduard Karulis <karulis@mail.ru>
2016-12-05 15:54:49 +0000

    DNX Last
    git-tfs-id: [https://karulis.visualstudio.com/DefaultCollection]$/SFCSlutskWebSite3;C89

--- s1/FCWebSite/src/FCWeb/gulpfile.js
+++ s1/FCWebSite/src/FCWeb/gulpfile.js
@@ -9,19 +9,69 @@ var gulp = require("gulp"),
     less = require("gulp-less"),
     project = require('./project.json');
 
+var wwwroot = "./wwwroot/";
+
 var paths = {
-    webroot: "./wwwroot/"
+    webroot: wwwroot,
+    fclib: wwwroot + "lib/fc/"
 };
 
-paths.js = paths.webroot + "js/**/*.js";
+//paths.js = paths.webroot + "js/**/*.js";
+paths.js = paths.webroot + "lib/fc/**/*.js";
 paths.minJs = paths.webroot + "js/**/*.min.js";
 paths.css = paths.webroot + "css/**/*.css";
 paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/site.min.js";
+paths.concatJsOfficeDest = paths.webroot + "js/site.office.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
+
+paths.fc = {    
+    js: {
+        front: [
+            paths.webroot + "lib/angular-route/angular-route.js",
+            paths.webroot + "lib/angular-translate/angular-translate.js",
+            paths.webroot + "lib/angular-translate-storage-local/angular-translate-storage-local.js",
+            paths.webroot + "lib/angular-translate-loader-static-files/angular-translate-loader-static-files.js",
+            paths.webroot + "lib/angular-sanitize/angular-sanitize.js",
+            paths.webroot + "lib/angular-animate/angular-animate.js",
+            paths.webroot + "lib/ng-image-gallery/dist/ng-image-gallery.js",
+
+            paths.fclib + "modules/fc.ui.js",
+            paths.fclib + "modules/fc.core.js",
+            paths.fclib + "app.js",
+            "!" + paths.fclib + "office/**/*.js",
+            "!" + paths.fclib + "layout/office/**/*.js",
+            paths.fclib + "**/*.js",
+        ],
+        office: [
+            paths.webroot + "lib/angular-route/angular-route.js",
+            paths.webroot + "lib/angular-translate/angular-translate.js",
+            paths.webroot + "lib/angular-translate-storage-local/angular-translate-storage-local.js",
+            paths.webroot + "lib/angular-translate-loader-static-files/angular-translate-loader-static-files.js",
+            paths.webroot + "lib/angular-sanitize/angular-sanitize.js",
+            paths.webroot + "lib/angular-animate/angular-animate.js",
+            paths.webroot + "lib/angular-ui-bootstrap/ui-bootstrap-tpls-1.2.4.js",
+            paths.webroot + "lib/angular-ui-bootstrap-datetimepicker/datetimepicker.js",
+            paths.webroot + "lib/angular-ui-numeric-master/src/angular-ui-numeric.js",
+            paths.webroot + "lib/ng-file-upload/ng-file-upload.js",
+            paths.webroot + "lib/ng-file-upload/ng-file-upload-shim.js",
+            paths.webroot + "lib/simple-angular-autocomplete-master/src/simple-autocomplete.js",
+            paths.webroot + "lib/ckeditor/ckeditor.js",
+
+            paths.fclib + "modules/fc.ui.js",
+            paths.fclib + "modules/fc.core.js",
+            paths.fclib + "appAdmin.js",
+            paths.fclib + "modules/**/*.js",
+            paths.fclib + "office/**/*.js",
+            paths.fclib + "layout/office/**/*.js",
+            paths.fclib + "**/*.js",
+        ]
+    }
+};
 
 gulp.task("clean:js", function (cb) {
     rimraf(paths.concatJsDest, cb);
+    //rimraf(paths.concatJsOfficeDest, cb);
 });
 
 gulp.task("clean:css", function (cb) {
@@ -30,12 +80,28 @@ gulp.task("clean:css", function (cb) {
 
 gulp.task("clean", ["clean:js", "clean:css"]);
 
-gulp.task("min:js", function () {
-    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+//gulp.task("min:js", function () {
+//    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+//        .pipe(concat(paths.concatJsDest))
+//        .pipe(uglify())
+//        .pipe(gulp.dest("."));
+//});
+
+gulp.task("min:jsfront", function () {
+    return gulp.src(paths.fc.js.front, { base: "." })
         .pipe(concat(paths.concatJsDest))
         .pipe(uglify())
         .pipe(gulp.dest("."));
 });
+
+gulp.task("min:jsoffice", function () {
+    return gulp.src(paths.fc.js.office, { base: "." })
+        .pipe(concat(paths.concatJsOfficeDest))
+        //.pipe(uglify())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min:js", ["min:jsfront", "min:jsoffice"]);
 
 gulp.task("min:css", function () {
     return gulp.src([paths.css, "!" + paths.minCss])
---
+++ s1/FCWebSite/src/FCWeb/wwwroot/js/personCtrl.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('fc')
+        .controller('personCtrl', personCtrl);
+
+    personCtrl.$inject = ['$scope', '$routeParams'];
+
+    function personCtrl($scope, $routeParams) {
+        $scope.personId = $routeParams.id;
+    }
+})();
