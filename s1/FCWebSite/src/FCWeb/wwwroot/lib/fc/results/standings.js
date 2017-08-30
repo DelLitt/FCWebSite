305cf7469a058abf531f22797ea849bdc65cb8a0
Eduard Karulis <karulis@mail.ru>
2017-08-30 14:49:12 +0000

    Results standings page fixes
    git-tfs-id: [https://karulis.visualstudio.com/DefaultCollection]$/SFCSlutskWebSite3;C116

--- s1/FCWebSite/src/FCWeb/wwwroot/lib/fc/results/standings.js
+++ s1/FCWebSite/src/FCWeb/wwwroot/lib/fc/results/standings.js
@@ -13,10 +13,52 @@
             $scope.title = translation;
         });
 
-        $scope.tourneyOptions = [
-            { index: 0, team: "MAIN_TEAM", tourneyId: configSrv.Current.MainTeamTourneyIds[2] },
-            { index: 1, team: "RESERVE_TEAM", tourneyId: configSrv.Current.ReserveTeamTourneyIds[0] }
-        ]
+        $scope.seasonOptions = [
+            {
+                index: 0,
+                season: "2017",
+                teamOptions:
+                  [
+                      { index: 0, team: "MAIN_TEAM", tourneyId: 14 },
+                      { index: 1, team: "RESERVE_TEAM", tourneyId: 118 }
+                  ]
+            },
+            {
+                index: 1,
+                season: "2016",
+                teamOptions:
+                  [
+                      { index: 0, team: "MAIN_TEAM", tourneyId: 12 }
+                  ]
+            },
+            {
+                index: 1,
+                season: "2015",
+                teamOptions:
+                  [
+                      { index: 0, team: "MAIN_TEAM", tourneyId: 10 }
+                  ]
+            }
+,
+            {
+                index: 1,
+                season: "2014",
+                teamOptions:
+                  [
+                      { index: 0, team: "MAIN_TEAM", tourneyId: 8 }
+                  ]
+            }
+        ];
+
+        $scope.$watch(function (scope) {
+            return $scope.seasonOpt;
+        },
+        function (newValue, oldValue) {
+            if (angular.isObject(newValue)) {
+                $scope.teamOptions = newValue.teamOptions;
+                $scope.teamOpt = $scope.teamOptions[0];
+            }
+        });
 
         $scope.teamId = configSrv.Current.MainTeamId;
         $scope.hasData = false;
